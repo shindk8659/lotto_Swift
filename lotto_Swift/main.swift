d89ffae034a3ec1958d9ecd798a9f36470226f1d
신동규 <shindk8659@naver.com>
2019-03-10 15:32:14 +0900

    CCStudy 로또문제 프로젝트 생성

---
+++ lotto_Swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  lotto_Swift
+//
+//  Created by 신동규 on 3/10/19.
+//  Copyright © 2019 CCStudy. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
---
+++ lotto_Swift_UnitTest/lotto_Swift_UnitTest.swift
@@ -0,0 +1,33 @@
+//
+//  lotto_Swift_UnitTest.swift
+//  lotto_Swift_UnitTest
+//
+//  Created by 신동규 on 3/10/19.
+//  Copyright © 2019 CCStudy. All rights reserved.
+//
+
+import XCTest
+
+class lotto_Swift_UnitTest: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
